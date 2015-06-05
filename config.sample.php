f50d1fd98024584a1a357ec0daf05e4fed6035c9
pp19dd <pp19dd@gmail.com>
2015-06-05 15:56:56 -0400

    Added db system.

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,12 @@
 <?php
 
 // no trailing slash
-define( "HOMEPAGE", "http://localhost/tour" );
+define( "HOMEPAGE",         "http://localhost/tour" );
+
+// don't allow direct-access to files
+define( "PROGRAM_RUNNING",  true );
+
+define( "MYSQL_HOST",       "localhost" );
+define( "MYSQL_USER",       "username" );
+define( "MYSQL_PASS",       "password" );
+define( "MYSQL_DATABASE",   "database" );
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-require( "vendor/autoload.php" );
 require( "config.php" );
+require( "vendor/autoload.php" );
+require( "class.db.php" );
 if( !defined( "HOMEPAGE") ) die( "Error, config file missing?" );
+
+$db = new VOA_DB(
+    MYSQL_HOST,
+    MYSQL_DATABASE,
+    MYSQL_USER,
+    MYSQL_PASS
+);
+
+// $t = $db->Query("show tables");
 
 $smarty = new Smarty();
 $smarty->assign( "homepage", HOMEPAGE );
